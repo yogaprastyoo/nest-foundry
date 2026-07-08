f8602adcb22b3f467cc61cea05fa7b04eaa390e7
Yoga Prastyo <yogaprastyobayu@gmail.com>
2026-07-09 03:28:12 +0700

    refactor: use ConfigService for database url in PrismaService

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,17 +1,19 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { Env } from '../config/env.validation';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  constructor() {
+  constructor(configService: ConfigService<Env, true>) {
     const adapter = new PrismaPg({
-      connectionString: process.env.DATABASE_URL,
+      connectionString: configService.get('DATABASE_URL', { infer: true }),
     });
 
     super({
       adapter,
-    } as any);
+    });
   }
 
   async onModuleInit() {
